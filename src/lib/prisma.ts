37a41a010c7f6b867071d88aa2ff90d552452383
emanuelsilvapereira <emanuelpereirasilvahenrique@gmail.com>
2026-03-08 23:25:37 -0300

    ajustes finais, projeto pronto, rodando na nuvem

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,29 +1,15 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaMariaDb } from '@prisma/adapter-mariadb'
+import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  const dbUrl = process.env.DATABASE_URL;
-
-  if (!dbUrl) {
-    throw new Error("A variável DATABASE_URL não foi encontrada. Verifique seu arquivo .env");
-  }
-
-  // Truque: O adaptador exige o prefixo mariadb:// para funcionar
-  const adapterUrl = dbUrl.replace('mysql://', 'mariadb://');
-  
-  // 1. Criamos a conexão do adaptador
-  const adapter = new PrismaMariaDb(adapterUrl);
-
-  // 2. Passamos o adaptador para o Prisma (o erro vermelho vai sumir!)
-  return new PrismaClient({ adapter })
-}
+  return new PrismaClient();
+};
 
 declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma
+export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
